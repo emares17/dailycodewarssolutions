4d88c9a0a565b25888d766e3e56cd087e0089a0a
Esteban Mares <97928534+emares17@users.noreply.github.com>
2022-04-27 19:05:31 -0700

    Add files via upload

---
+++ 04-27-2022.js
@@ -0,0 +1,16 @@
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+// -----------------Solution------------------
+const smash = words => words.join(" ");
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Example:
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+// -------------Solution---------------------
+const reverseWords = (str) => str.split(" ").reverse().join(" ");
