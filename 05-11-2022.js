de17ca3b569dadeee48295672443ce6e2a94b2d7
Esteban Mares <97928534+emares17@users.noreply.github.com>
2022-05-11 19:03:50 -0700

    Add files via upload

---
+++ 05-11-2022.js
@@ -0,0 +1,7 @@
+// Task:
+
+// Check if the input ( one letter ) is Uppercase with no more than 9 characters.
+
+// No need to check for invalid input. Only letters will be tested. Return true if Uppercase and false if Lowercase.
+// ----------------------Solution-----------------------
+f=s=>s<{}
