c9d566b070d23c832424ced839f31bca33d2fc52
Esteban Mares <97928534+emares17@users.noreply.github.com>
2022-08-03 18:54:16 -0700

    Add files via upload

---
+++ 08-03-2022.js
@@ -0,0 +1,7 @@
+// Remove String Spaces
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+// -------------------------Solution--------------------------
+
+const noSpace = x => x.replace(/ /g, '');
