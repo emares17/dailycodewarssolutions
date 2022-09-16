998849d9654322ee56fd5d82b9d55316737ebd90
Esteban Mares <97928534+emares17@users.noreply.github.com>
2022-09-15 20:46:24 -0700

    Add files via upload

---
+++ 09-15-2022.js
@@ -0,0 +1,18 @@
+// Make acronym
+
+// Write function which takes a string and make an acronym of it.
+
+// Rule of making acronym in this kata:
+
+// split string to words by space char
+// take every first letter from word in given string
+// uppercase it
+// join them toghether
+// Eg:
+
+// Code wars -> C, w -> C W -> CW
+// Note: There will be at least two words in the given string!
+
+// -------------------------Solution--------------------------
+
+const toAcronym = inp => inp.split(' ').map(element => element[0].toUpperCase()).join('');
