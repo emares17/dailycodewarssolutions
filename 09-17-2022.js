da6eea05b68b13a6a35c3c927018b6848aa49fbe
Esteban Mares <97928534+emares17@users.noreply.github.com>
2022-09-17 23:40:37 -0700

    Add files via upload

---
+++ 09-17-2022.js
@@ -0,0 +1,13 @@
+// Filter Long Words
+
+// Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
+
+// Example:
+
+// * With input "The quick brown fox jumps over the lazy dog", 4
+// * Return ['quick', 'brown', 'jumps']
+
+// ----------------------Solution-----------------------
+
+const filterLongWords = (sentence, n) => sentence.split(' ').filter(v => v.length > n);
+
