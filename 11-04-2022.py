40a12170adcc54ff7827869170c7801b131c0074
Esteban Mares <97928534+emares17@users.noreply.github.com>
2022-11-04 19:31:12 -0700

    Add files via upload

---
+++ 11-04-2022.py
@@ -0,0 +1,8 @@
+# Remove exclamation marks
+
+# Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+# ---------------------------Solution--------------------------------
+
+def remove_exclamation_marks(s):
+    return s.replace('!', '')
