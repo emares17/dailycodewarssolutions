0def7c72c3a16b6626e6094bbf9a941602770c39
Esteban Mares <97928534+emares17@users.noreply.github.com>
2022-11-15 17:44:08 -0800

    Add files via upload

---
+++ 11-15-2022.py
@@ -0,0 +1,19 @@
+# Find the unique number
+
+# DESCRIPTION:
+# There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+# find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+# find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+# It’s guaranteed that array contains at least 3 numbers.
+
+# The tests contain some very huge arrays, so think about performance.
+
+# -------------------------------------Solution----------------------------------------
+
+from collections import Counter
+
+def find_uniq(arr):
+    for v, i in Counter(arr).items():
+        if i == 1:
+            return v
