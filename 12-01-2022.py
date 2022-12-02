c4ee60d7a505a5fd90e7d8fbf63937e22c7b4401
Esteban Mares <97928534+emares17@users.noreply.github.com>
2022-12-01 20:44:39 -0800

    Add files via upload

---
+++ 12-01-2022.py
@@ -0,0 +1,13 @@
+# Largest Elements
+
+# Write a program that outputs the top n elements from a list.
+
+# Example:
+
+# largest(2, [7,6,5,4,3,2,1])
+## => [6,7]
+
+# --------------------------------Solution----------------------------------
+
+def largest(n,xs):
+    return sorted(xs)[-n:]
