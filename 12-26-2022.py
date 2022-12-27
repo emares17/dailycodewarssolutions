963d96f7f56fa177c06d54a7aa5cc30f846c418b
Esteban Mares <97928534+emares17@users.noreply.github.com>
2022-12-26 20:00:24 -0800

    Add files via upload

---
+++ 12-26-2022.py
@@ -0,0 +1,38 @@
+# Alternate Square Sum
+
+# Complete the function that takes an array of integers as input and finds the sum of squares of the elements 
+# at even positions (i.e. 2nd, 4th, etc.) plus the sum of the rest of the elements at odd position.
+
+# For empty arrays, result should be zero (except for Haskell).
+
+# Note
+# The values at even positions need to be squared. For a language with zero-based indices, this will occur at 
+# oddly-indexed locations. For instance, in Python, the values at indices 1, 3, 5, etc. should be squared because these are the second, fourth, and sixth positions in the list.
+
+# Example
+# [11, 12, 13, 14, 15]  -->  379
+# # 1.  2.  3.  4.  5.  position
+
+# 11 + 12^2 + 13 + 14^2 + 15 = 379
+# Explanation:
+
+# Elements at indices 0, 2, 4 are 11, 13, 15 and they are at odd positions as 11 is at position 
+# #1, 13 is at position #3 and 15 at #5.
+
+# Elements at indices 1, 3 are 12 and 14 and they are at even position. So we need to add 11, 13, 15 
+# as they are and square of 12 and 14
+
+# -----------------------------------Solution-----------------------------------
+def alternate_sq_sum(arr):
+    return sum(c ** 2 if i % 2 == 1 else c for i, c in enumerate(arr))
+
+# -----------------------------------Or-----------------------------------------
+
+def alternate_sq_sum(arr):
+    stack = []
+    for i, c in enumerate(arr):
+        if i % 2 == 1:
+            stack.append(c ** 2)
+        else:
+            stack.append(c)
+    return sum(stack)
