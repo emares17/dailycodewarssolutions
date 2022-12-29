ed8a1b505c7515c8ff0e86774f0ad7e009ace757
Esteban Mares <97928534+emares17@users.noreply.github.com>
2022-12-28 19:07:21 -0800

    Add files via upload

---
+++ 12-28-2022.py
@@ -0,0 +1,13 @@
+# String ends with?
+
+# Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+# Examples:
+
+# solution('abc', 'bc') # returns true
+# solution('abc', 'd') # returns false
+
+# ----------------------------------Solution-------------------------------
+
+def solution(string, ending):
+    return True if string[-len(ending):] == ending or ending == '' else False
