fba0613230400b4c22cb744ff9c1ffb6d1a37c9a
Esteban Mares <97928534+emares17@users.noreply.github.com>
2023-01-24 18:40:00 -0800

    Add files via upload

---
+++ 01-24-2023.py
@@ -0,0 +1,22 @@
+# Snail crawls up
+
+# The snail crawls up the column. During the day it crawls up some distance. 
+# During the night she sleeps, so she slides down for some distance (less than crawls up during the day).
+
+# Your function takes three arguments:
+
+# The height of the column (meters)
+# The distance that the snail crawls during the day (meters)
+# The distance that the snail slides down during the night (meters)
+# Calculate number of day when the snail will reach the top of the column.
+
+# ---------------------------Solution---------------------------
+
+def snail(column, day, night):
+    count = 0
+    while column > 0:
+        column -= day
+        count += 1
+        if column > 0:
+            column += night
+    return count
